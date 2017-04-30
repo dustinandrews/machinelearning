3fe8f8959d808f1b9465e2d0d051ecfb0286c58e
dustinandrews <dustinandrews@hotmail.com>
2017-04-29 19:41:36 -0700

    ttyrec downloader working

--- ttyrec/recordings/get_recordings.py
+++ ttyrec/recordings/get_recordings.py
@@ -43,7 +43,7 @@ class ttyrec_download():
             f_name = self.get_file_path(url)
             if os.path.exists(f_name):
                 with open(f_name, "r") as f:
-                    return f.readlines()
+                    return f.read()
         else:
             os.mkdir(self.cache_dir)
             return None
@@ -63,25 +63,66 @@ class ttyrec_download():
             player_urls = [h['href'] for h in soup.find_all('a', href=True) if h['href'].startswith('plr')]
             self.cache_page(cache_key, "\n".join(player_urls))
         else:
-            player_urls = [line.rstrip() for line in player_urls]
+            player_urls = player_urls.split("\n")
 
         return player_urls
     
-    def save_player_page(self, player):
-        name = player.split("=")[1]
-        ttypage = 'browsettyrec.php?player=' + name
+    
+    def get_player_dir(self, name):
+        return self.recording_dir + "/" + name
+        
+    def get_player_ttyrec_index(self, player_name):        
+        ttypage = 'browsettyrec.php?player=' + player_name
         url = self.base_url + ttypage
         print(url)
-        if self.check_cache(url) == None:
-            page = self.get_page(url)            
-            player_dir = self.recording_dir + "/" + name
-            os.makedirs(player_dir, exist_ok=True)
-            with open(player_dir + "/browsettyrec.html" , "w") as p:
+        page = self.get_page(url)            
+        player_dir = self.get_player_dir(player_name)
+        os.makedirs(player_dir, exist_ok=True)
+        with open(player_dir + "/browsettyrec.html" , "w") as p:
                 p.write(page)
+        return self.parse_tty_list_page(page)
+
+    def parse_tty_list_page(self, page):
+        strainer = SoupStrainer('a')
+        soup = BeautifulSoup(str(page), 'lxml', parse_only=strainer)
+        ttyrecs = [h['href'] for h in soup.find_all('a', href=True) if h['href'].startswith('https://s3.amazonaws.com/altorg/ttyrec/')]
+        return ttyrecs
+     
+    def download_binary(self, url, local_dir):
+        file_name = local_dir + "/" + url.split('/')[-1].replace(":", "_")
+        if not os.path.exists(file_name):
+            u = urlopen(url)
+            f = open(file_name, 'wb')
+            meta = u.info()            
+            file_size = int(meta["Content-Length"])
+            print( "Downloading: {} Bytes: {}".format(file_name, file_size))
+            
+            file_size_dl = 0
+            block_sz = 8192
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+            
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                status = status + chr(8)*(len(status)+1)
+                print (status, end="")       
+            f.close()
+        else:
+            print("Already have {}".format(file_name))
+        
 
 
 if __name__ == '__main__':
     dl = ttyrec_download()
     urls = dl.get_player_urls()
-        
+    player_url = urls[0]
+    
+    name = player_url.split("=")[1]
+    tty_index = dl.get_player_ttyrec_index(name)
+    for index in tty_index:
+        dl.download_binary(index, dl.get_player_dir(name))
+    
     
