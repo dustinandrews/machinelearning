9c72fcba3af9a6bf8f434d2069ec54459350d6ce
Dustin Andrews <dustinandrews@hotmail.com>
2017-05-16 15:48:43 -0700

    Bandit analizer working for data/label method.

--- BanditStudy.py
+++ BanditStudy.py
@@ -10,9 +10,11 @@ Created on Sun May 14 10:11:32 2017
 import numpy as np
 import cntk as C
 import collections
+import matplotlib.pyplot as plt
 
-
-Hyperparameters = collections.namedtuple('Hyperparamters', 'hidden_dim learning_rate minibatch_size epochs')
+Hyperparameters = collections.namedtuple(
+        'Hyperparamters',
+        'hidden_dim learning_rate minibatch_size epochs loss l1reg l2reg stop_loss')
 
 class Bandit:
     
@@ -36,7 +38,8 @@ class Solver:
     def __init__(self, num_arms: int,  hp: Hyperparameters):         
         self.bandit = Bandit(num_arms)
         self.input_var = C.input(1, dtype=np.float32)
-        self.output_var = C.input(num_arms, name="l_output")
+        self.output_var = C.input(num_arms, name="output_var")
+        self.label_var = C.input(num_arms, name="label_var")
         self.create_model(hp)
         self.actions = np.arange(num_arms, dtype=np.int32)
         self.softmax = C.softmax(self.output_var)
@@ -44,14 +47,14 @@ class Solver:
         self.truth = self.softmax.eval(np.array(self.bandit.arms, dtype=np.float32))
         self.hp = hp
         self.error = self.get_squared_error()
+        self.plotdata = {"loss":[]}
     
     def network(self, input_dim, hidden_dim, output_dim, input_var):
         
         #self.l1 = C.layers.Dense(hidden_dim, activation=C.sigmoid, name='l_hidden')(input_var)
-        #self.l2 = C.layers.Dense(output_dim, name='l_output', activation=C.sigmoid)(self.l1)
+        #self.l2 = C.layers.Dense(output_dim, name='output_var', activation=C.sigmoid)(self.l1)
         l1 = C.layers.Dense(hidden_dim, activation=C.sigmoid, name='l_hidden1')(input_var)
-        l2 = C.layers.Dense(output_dim, name='l_hidden2', activation=C.sigmoid)(l1)        
-        self.reverse_layers = ['l_output', 'l_hidden2', 'l_hidden1']        
+        l2 = C.layers.Dense(output_dim, name='l_hidden2', activation=C.sigmoid)(l1)               
         return l2
         
     def create_model(self, hp: Hyperparameters):
@@ -63,10 +66,26 @@ class Solver:
         val = np.random.choice(self.actions, p = prob)
         return val
     
+    """
+    Use for Reinforcement learning
+    """
     def step(self):
         action = self.get_action(self.in_data)
         reward = self.bandit.step(action)
         print(reward)
+        
+    """
+    Use for regular output/label learning
+    """
+    def get_next_data(self, size: int):        
+        indata = []
+        labeldata = []        
+        for _ in range(size):               
+            indata.append(np.array([np.random.choice(self.actions)], dtype=np.float32))
+            labeldata.append(np.array((self.truth), dtype=np.float32))         
+        indata = np.array(indata, dtype=np.float32)
+        labeldata = np.array(labeldata, dtype=np.float32)
+        return indata, labeldata
         
     def ln_derivative(self, layer):
         w =layer.W.value
@@ -102,19 +121,64 @@ class Solver:
         
         pass
     
-
-    def manual_train(self, iterations: int):
-        print("error:" + str(self.get_error()) )
-        for i in range(iterations):
-            self.backprop()
-            if i % 100 == 0:
-                print("error:" + str(self.get_squared_error()) )
-        print("error:" + str(self.get_squared_error()) )
-
+    def loss_funtion(self):
+        loss_fs = {
+                'squared_error': C.squared_error(self.model, self.label_var),
+                'cross_entropy_with_softmax':   C.cross_entropy_with_softmax(self.model, self.label_var, axis=0),
+                'binary_cross_entropy' : C.binary_cross_entropy(self.model, self.label_var),
+                }
+        return loss_fs[self.hp.loss]
+        
+    
     # TODO: Regular CNTK training and then work on making it do policy gradient.
     # The example from CNTK is a mess.
-    def train(self):
-        pass
+    def train(self, report_freq = 500):        
+        loss = self.loss_funtion()
+        evaluation = self.loss_funtion()
+        schedule = C.momentum_schedule(self.hp.learning_rate)
+        progress_printer = C.logging.ProgressPrinter(num_epochs=self.hp.epochs)
+        learner = C.adam(self.model.parameters, 
+                     C.learning_rate_schedule(self.hp.learning_rate, C.UnitType.minibatch), 
+                     momentum=schedule, 
+                     l1_regularization_weight=self.hp.l1reg,
+                     l2_regularization_weight=self.hp.l2reg
+                     )
+        trainer = C.Trainer(self.model, (loss, evaluation), learner, progress_printer)
+        self.plotdata = {"loss":[]}
+        for epoch in range(self.hp.epochs):             
+             indata, label = self.get_next_data(self.hp.minibatch_size)
+             data = {self.input_var: indata, self.label_var: label}
+             trainer.train_minibatch(data)
+             loss = trainer.previous_minibatch_loss_average
+             if not (loss == "NA"):
+                self.plotdata["loss"].append(loss)
+             if epoch % report_freq == 0:
+                 trainer.summarize_training_progress()
+             if self.hp.stop_loss > loss:
+                 break
+             
+    def plot_loss(self):                    
+        if len(self.plotdata["loss"]) > 100:
+            self.plotdata["avgloss"] = self.moving_average(self.plotdata["loss"], 100)
+        else:
+            self.plotdata["avgloss"] =self.plotdata["loss"]
+        #plotdata["avgloss"] = plotdata["loss"]
+        plt.figure(1)    
+        plt.subplot(211)
+        plt.plot(self.plotdata["avgloss"])
+        plt.xlabel('Minibatch number')
+        plt.ylabel('Loss')
+        #plt.yscale('log', basex=10)
+        plt.title('Minibatch run vs. Training loss')
+        plt.show()
+             
+        
+    def moving_average(self, a, n=3):
+        ret = np.cumsum(a, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        return ret[n - 1:] / n
+    
+        
     
 
 
@@ -122,19 +186,21 @@ class Solver:
     
 if __name__ == '__main__':
     
-    hp = Hyperparameters(hidden_dim=3, learning_rate=1e-1, minibatch_size=20, epochs=100)
+    hp = Hyperparameters(
+            hidden_dim=3,
+            learning_rate=1e-2,
+            minibatch_size=250,
+            epochs=10000,
+            loss='squared_error',
+            l1reg = 0,
+            l2reg = 0,
+            stop_loss = 1e-4
+            )
     solver = Solver(3, hp)
     self = solver
-    print(solver.error)
+    self.train()
+    self.plot_loss()
     
-    x = self.cross_check_backprop()    
-    l = self.model.find_by_name('l_hidden2')
-    xdelta = l.W.value - x
-    self.backprop()
-    bdelta = l.W.value - x
-    print(xdelta)
-    print(bdelta)
-    print(xdelta-bdelta)
     
     
     
