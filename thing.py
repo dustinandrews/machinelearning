8ef93835ddb7e7bc7374dfc5fb64ce522e3ffd54
dustinandrews <dustinandrews@hotmail.com>
2017-05-23 18:07:01 -0700

    checkpointing

--- thing.py
+++ thing.py
@@ -6,7 +6,10 @@ This is a temporary script file.
 """
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Embedding, Reshape, Convolution2D, Flatten, Lambda, MaxPooling2D, Conv2D, UpSampling2D 
+from keras.layers import Dense, Activation, Embedding, Reshape, Convolution2D
+from keras.layers import Flatten, Lambda, MaxPooling2D, Conv2D, UpSampling2D
+from keras.layers import Dropout
+from keras.optimizers import Adam
 from keras import regularizers
 
 import numpy as np
@@ -16,7 +19,8 @@ from daml.parameters import hyperparameters
 import random
 from matplotlib import pyplot as plt
 from keras import backend as K
-
+from keras.callbacks import ModelCheckpoint
+from keras.constraints import maxnorm
 
 #%%
 class KAutoEncoder:
@@ -27,21 +31,27 @@ class KAutoEncoder:
         self.dfile = datafile
         self.data = datafile.root.earray
         self.hp = hp
-        K.clear_session()
+        
+        
         
         
     def create_model(self, output_shape):
+        K.clear_session()
         self.output_shape = output_shape
         model = Sequential([
-                Dense(self.hp.hidden_dim, activation='relu', input_shape=((1,) + output_shape)),
-#               Conv2D(8, (1,1), activation='relu', padding='same',
-#                      input_shape=(1,24,80),data_format="channels_first",
-#                      ),
+               Dropout(0.15, input_shape=(1,24,80)),
+               Conv2D(8, (1,1), activation='relu', padding='same',
+                      input_shape=(1,24,80),data_format="channels_first",
+                      ),
+               Dropout(0.15),
                Flatten(),
-               Dense(24*80, activation='relu',
-                     ),
+#               Dense(self.hp.hidden_dim, activation='relu'),
+               Dense(self.hp.hidden_dim, activation='relu',kernel_constraint=maxnorm(3)),
+               Dropout(0.15),
+               Dense(24*80, activation='relu',kernel_constraint=maxnorm(3)),
                Reshape((1,24,80)),
             ])
+               
         model.compile(optimizer=self.hp.optimizer,
                       loss=self.hp.loss,
                       metrics=['accuracy'])
@@ -62,17 +72,20 @@ class KAutoEncoder:
     
     
     def train_model(self, model = None):
+#        filepath="weights-best.hdf5"
+#        checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='max')
+#        callbacks_list = [checkpoint]
         sample = 2048
-        batch = 64
         if model == None:
             self.model = k.create_model((24,80))
         x_train = self.get_samples(sample)
         y_train = x_train
-        history = self.model.fit(x_train, y_train, epochs=self.hp.epochs, batch_size=batch)        
-        x_test = self.get_samples(128)
-        y_test = x_test
-        score = self.model.evaluate(x_test, y_test, batch_size=128)
-        print("Score {}, Sample Size:{} Batch Size:{}".format(score,batch,sample))
+        history = self.model.fit(x_train, y_train, epochs=self.hp.epochs, batch_size=self.hp.minibatch_size, verbose=0) 
+#        history = self.model.fit(x_train, y_train, epochs=self.hp.epochs, batch_size=batch, callbacks=callbacks_list)        
+#        x_test = self.get_samples(128)
+#        y_test = x_test
+#        score = self.model.evaluate(x_test, y_test, batch_size=128)
+#        print("Score {}, Sample Size:{} Batch Size:{}".format(score,self.hp.minibatch_size,sample))
         return history.history
             
     def plot_loss(self, loss, name):                            
@@ -89,14 +102,16 @@ class KAutoEncoder:
     
     
 if __name__ == '__main__':
+
     hp = hyperparameters(
-             hidden_dim=24*80, 
-             learning_rate=1e-3, 
+             hidden_dim=int(24*80*2), 
+             learning_rate=1e-4, 
              minibatch_size=200,
-             epochs=100,
-             optimizer='adam',
-             loss='mean_squared_error'
+             epochs=75,
+             optimizer='rmsprop',
+             loss='mse'
              )
+    
 
     def eval():
         x = k.get_samples(1)
@@ -112,10 +127,10 @@ if __name__ == '__main__':
         print('closed file')
     k = KAutoEncoder(hp)
 
-    history = k.train_model()
-    eval()
+#    history = k.train_model()
+#    eval()
     h = []
-    num_tests = 1
+    num_tests = 3
     for i in range(num_tests):    
         history = k.train_model()
         h.append(history)
@@ -125,7 +140,8 @@ if __name__ == '__main__':
         run[i] = np.max(h[i]['acc'])
 
     print("mean: {} median: {} std: {}".format(np.mean(run), np.median(run), np.std(run)))
-
+    record[opt] = np.median(run)
+    
 #    k.plot_loss(history['loss'], 'loss')
 #    k.plot_loss(history['acc'], 'acc')
 #    eval()
