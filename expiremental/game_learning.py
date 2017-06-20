fab72052a6d0b509269eb87a11ee4cb99ae099b7
dustinandrews <dustinandrews@users.noreply.github.com>
2017-06-20 12:16:13 -0700

    Experimenting with different models

--- expiremental/game_learning.py
+++ expiremental/game_learning.py
@@ -14,19 +14,22 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Conv2D, Conv2DTranspose
 from keras.layers import Dropout
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau
 import numpy as np
 from matplotlib import pyplot as plt
+from keras.optimizers import Adam
+from keras import regularizers
 
 import sys
 if r'D:\local\machinelearning' not in sys.path:
     sys.path.append(r'D:\local\machinelearning')
-from daml.parameters import hyperparameters
+from daml.parameters import hyperparameters    
+
 
 class game_learn:
     
-    def __init__(self, hp: hyperparameters):
-        data_file = tables.open_file('processed_06_09.h5')
+    def __init__(self, hp: hyperparameters, data_file_name: str):
+        data_file = tables.open_file(data_file_name)
         self.hp = hp
         self.data_file = data_file
         self.data = data_file.root.data
@@ -45,7 +48,10 @@ class game_learn:
                              verbose=0,
                              save_best_only=True,
                              mode='auto')
+        self.early_stop = EarlyStopping(monitor='loss', patience=100, mode='auto')
         self.epoch = 0
+        self.tbCallBack = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+        self.reduce_lr_callback = ReduceLROnPlateau()
 
 
     def create_model_autoencoder(self):
@@ -74,27 +80,55 @@ class game_learn:
               metrics=['accuracy'])
         return model
 
-    def create_model_classify(self):
+    def create_model_classify_from_autoencoder(self, weights_file):
+       
+        model = self.create_model_classify(compile_model=False)
+        model.load_weights(weights_file, by_name=True)
+
+#        for layer in model.layers:
+#            if 'conv2d' in layer.name:
+#                layer.trainable = False
+        model.compile(optimizer=self.hp.optimizer,
+              loss=self.hp.loss,
+              metrics=['accuracy'])
+        model.summary()
+        return model
+    
+    def create_model_classify(self, compile_model = True):
         print("creating classify model")
+        data_format="channels_last"
+        activation = 'tanh'
         K.clear_session()
         model = Sequential()
         model.add(Lambda(lambda x: x/127.5-1.0, input_shape=self.input_shape ))
-        model.add(Conv2D(24, (1, 1), activation='elu', strides=(2, 2), data_format="channels_first", input_shape=self.input_shape))
-        model.add(Conv2D(36, (5, 5), activation='elu', strides=(2, 2), data_format="channels_first"))
-        model.add(Conv2D(48, (5, 5), activation='elu', strides=(2, 2), data_format="channels_first"))
-        model.add(Conv2D(64, (3, 3), activation='elu', data_format="channels_first"))
-        model.add(Conv2D(64, (3, 3), activation='elu', data_format="channels_first"))
-        model.add(Dropout(hp.dropout))
+        #model.add(Dropout(hp.dropout))
+        model.add(Conv2D(24, (5, 5), activation=activation, strides=(2, 2), data_format=data_format,kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        model.add(Conv2D(36, (5, 5), activation=activation, strides=(2, 2), data_format=data_format, kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        #model.add(Dropout(hp.dropout))
+        model.add(Conv2D(48, (5, 5), activation=activation, strides=(2, 2), data_format=data_format,kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        model.add(Conv2D(64, (3, 3), activation=activation, data_format=data_format, kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        model.add(Conv2D(64, (3, 3), activation=activation, data_format=data_format, kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        #model.add(Dropout(hp.dropout))
         model.add(Flatten())
-        model.add(Dense(100, activation='elu'))
-        model.add(Dense(50, activation='elu'))
-        model.add(Dense(10, activation='elu'))
-        model.add(Dense(self.output_shape[0], activation='sigmoid'))
-        model.summary()
+        model.add(Dense(100, activation=activation,kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+        model.add(Dense(50, activation=activation))
+        model.add(Dense(10, activation=activation))
+        model.add(Dense(self.output_shape[0], activation='tanh', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
         
-        model.compile(optimizer=self.hp.optimizer,
-              loss=self.hp.loss,
-              metrics=['accuracy'])        
+        
+        if compile_model:
+            model.summary()
+            model.compile(
+                  optimizer= Adam(clipnorm = .001, lr=self.hp.learning_rate),
+                  loss=self.hp.loss,)
+                  #metrics=['loss'])        
         return model
     
     def get_samples(self, num_samples: int, train = True):
@@ -109,10 +143,10 @@ class game_learn:
                 index = self.data_index % len(self.test_order)
                 
             item = self.train_order[index]
+            #print(index,item)
             data[i] = self.data[item]
             labels[i] = self.labels[item]
-            index += 1
-        self.data_index = index
+            self.data_index += 1
         return data, labels
     
     
@@ -140,24 +174,29 @@ class game_learn:
     
     
     
-#    def train_model(self, model = None):
-#        sample = 1024
-#        if model == None:
-#            self.model = self.create_model_classify()
-#        x_train, y_train = self.get_samples(sample)
-#        x_test, y_test = self.get_samples(512, train=False)
-#        history = self.model.fit(                
-#                x_train, 
-#                y_train,
-#                callbacks=[self.checkpoint],
-#                epochs=self.hp.epochs, 
-#                batch_size=self.hp.minibatch_size, 
-#                verbose=1,
-#                validation_data=(x_test, y_test),
-#                initial_epoch=self.epoch
-#                )
-#        self.epoch += self.hp.epochs
-#        return history.history
+    def train_model(self, model = None):
+        sample = 1024
+        if model == None:
+            self.model = self.create_model_classify()
+        x_train, y_train = self.get_samples(sample)
+        x_test, y_test = self.get_samples(512, train=False)
+        history = self.model.fit(                
+                x_train, 
+                y_train,
+                callbacks=[
+                        self.checkpoint,  
+                        self.tbCallBack, 
+                        self.reduce_lr_callback,
+                        self.early_stop,
+                        ],
+                epochs=self.hp.epochs, 
+                batch_size=self.hp.minibatch_size, 
+                verbose=1,
+                validation_data=(x_test, y_test),
+                initial_epoch=self.epoch
+                )
+        self.epoch += self.hp.epochs
+        return history.history
             
     def plot_data(self, data, name):                            
         #plotdata["avgloss"] = plotdata["loss"]
@@ -170,7 +209,7 @@ class game_learn:
         plt.title('Minibatch run vs. ' + name)
         plt.show()        
     
-    
+
     
     def cleanup(self):
         self.data_file.close()
@@ -216,51 +255,82 @@ class game_learn:
             ret_val[names[i]] = activations[i]
         return ret_val
 
+    def create_new_inception_based_model(self):
+        with open('inception.json', 'r') as j:
+                inception_json = j.read()       
+        inception_model = models.model_from_json(inception_json)
+        inception_out_shape = inception_model.layers[-1].output.shape.as_list()
+        inception_out_shape = tuple(inception_out_shape)
+        output_shape = 4
+        # Add custom classifier on the top
+        x = inception_model.layers[-1].output
+        #x = concatenate(axis=-1)(x)
+        x = Flatten()(x)
+        x = Dense(100, activation='elu', kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01))(x)
+        x = Dense(50, activation='elu')(x)
+        x = Dense(10, activation='elu')(x)
+        x = Dense(output_shape, kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01))(x)
+        model = models.Model(inception_model.layers[0].input, x)
+        model.compile(hp.optimizer, hp.loss)
+        return model
+        
+
+    def load_latest_model(self):
+        model_saves = glob.glob("model*")
+        if len(model_saves) > 0:
+            file_name = glob.glob("model*")[-1]
+            self.model = models.load_model(file_name)
+            m = re.search('\d+', file_name)  
+            self.epoch = int(m.group(0))
+        else:
+            raise FileNotFoundError('No models to load')
+            
+    def show_random_prediction(self):
+        x,y = self.get_samples(1)
+        p = self.model.predict(x)
+        plt.imshow(x.reshape(299,299), 'gray')
+        plt.show()
+        print('Predicted', p[0])
+        print('Recorded', y[0])
+        delta = p[0] - y[0]
+        print('Delta', delta)
+        return delta
+
 #%%    
 if __name__ == '__main__':
 
-
+    K.set_image_data_format('channels_last')
     import glob
-    import re
+    import re    
     hp = hyperparameters()
-    hp.epochs = 1000
+    hp.epochs = 100
     hp.minibatch_size = 10
     hp.optimizer = 'adam'
+    hp.learning_rate = 1e-4
     hp.loss = 'mse'
-    hp.dropout = 0.50
+    hp.dropout = 0.20
     
-    tl = game_learn(hp)
-    model_saves = glob.glob("model*")
-    if len(model_saves) == 0:
-        tl.model = tl.create_model_autoencoder()
-    else:
-        file_name = glob.glob("model*")[-1]
-        tl.model = models.load_model(file_name)
-        m = re.search('\d+', file_name)  
-        tl.epoch = int(m.group(0))
-        
-    history = tl.train_autoencoder(tl.model)
-    plt.plot(history['loss'])
-    plt.show()
+#    tl = game_learn(hp, 'game_data_four_label.h5')
+    tl = game_learn(hp, 'game_data_five.h5')
 
-    x,y = tl.get_samples(1)
-    p = tl.model.predict(x)
-    plt.imshow(x[0][0])
-    plt.show()
-    plt.imshow(p[0][0])
-    plt.show()
-      
+#%%
+#    tl.load_latest_model();
+#    history = tl.train_model(tl.model)
+
+
     
 #%%
-    x,y = tl.get_samples(1)
-    activations = tl.get_activations(x, True, 'conv')
+#    x,y = tl.get_samples(1)
+#    activations = tl.get_activations(x, True, 'conv')
+#
+#    for name in sorted(activations):
+#        print('\n\n')
+#        print(name)
+#        fig = plt.gcf()
+#        fig.set_dpi(150)
+#        plt.imshow(activations[name][0][0], 'gray')
+#        plt.show() 
 
-    for name in sorted(activations):
-        print('\n\n')
-        print(name)
-        fig = plt.gcf()
-        fig.set_dpi(150)
-        plt.imshow(activations[name][0][0], 'gray')
-        plt.show() 
-        
-          
+      
